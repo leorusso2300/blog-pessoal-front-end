768f03690c56f05cca5a5d3300fc88d0c869fd72
leorusso2300 <leorusso2607@gmail.com>
2021-12-08 10:38:23 -0300

    mudanças

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -16,22 +16,22 @@ export class PostagemService {
   }
 
   getAllPostagens(): Observable<Postagem[]>{
-    return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
+    return this.http.get<Postagem[]>('https://blogdoleorusso.herokuapp.com/postagens', this.token)
   }
 
   getByidPostagem(id: number): Observable<Postagem>{
-    return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
+    return this.http.get<Postagem>(`https://blogdoleorusso.herokuapp.com/postagens/${id}`, this.token)
   }
 
   postPostagem(postagem: Postagem) : Observable<Postagem>{
-    return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.post<Postagem>('https://blogdoleorusso.herokuapp.com/postagens', postagem, this.token)
   }
 
   putPostagem(postagem: Postagem): Observable<Postagem>{
-    return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.put<Postagem>('https://blogdoleorusso.herokuapp.com/postagens', postagem, this.token)
   }
 
   deletePostagem(id: number){
-    return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
+    return this.http.delete(`https://blogdoleorusso.herokuapp.com/postagens/${id}`, this.token)
   }
 }
